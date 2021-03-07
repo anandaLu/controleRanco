d122b114afec32c2932c97e0d5659da0692b12cd
Ananda Teixeira <anandanemesio@gmail.com>
2021-03-07 19:53:25 -0300

    sevlet criado

---
+++ src/main/java/br/upe/piii/controleRanco/ControleRancoApplication.java
@@ -0,0 +1,13 @@
+package br.upe.piii.controleRanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleRancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleRancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/upe/piii/controleRanco/ControleRancoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.piii.controleRanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleRancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
